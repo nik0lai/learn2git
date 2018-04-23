300d43d43f8bee691c3e151473b987ed484a3711
Nicolas Sanchez <nicolas.csanchez@gmail.com>
2018-04-22 21:11:56 -0300

    remove R installation parts

---
+++ scripts/old_r.R
@@ -0,0 +1,12 @@
+# 
+# ## Instala R para tu sistema operativo.
+# * [Windows](https://cloud.r-project.org/bin/windows/base/)
+# * [Mac OS X](https://cloud.r-project.org/bin/macosx/)
+# * [Linux](https://cloud.r-project.org/)
+# 
+# Instala [R Studio](https://www.rstudio.com/products/rstudio/download/preview/).
+# * Bajo "Desktop Version" encontrarás las versiones correspondientes para Windows Vista/7/8/10, Mac OS X, y Linux.
+# 
+# Actualiza los paquetes de R.
+# * Para actualizar los paquetes debes correr la siguiente linea en la consola de R Studio:
+#   `update.packages(ask = FALSE, checkBuilt = TRUE)`
