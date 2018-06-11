2b35656ca90a0b2bb242a5e35c471210bccb40f2
Nicolas Sanchez <nicolas.csanchez@gmail.com>
2018-06-11 01:52:50 -0400

    debug messages

--- _deploy.sh
+++ _deploy.sh
@@ -9,8 +9,13 @@ git config --global user.email "nicolas.csanchez@gmail.com"
 git config --global user.name "Nicolas Sanchez"
 
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
+"A"
 cd book-output
+"B"
 cp -r ../_book/* ./
+"C"
 git add --all *
+"D"
 git commit -m"Update the book" || true
+"E"
 git push -q origin gh-pages
